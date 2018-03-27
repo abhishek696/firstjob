742b66286d72f4ae4fc6593a3b47e2a8c2df300f
abhishek696 <abhishektyagi8324@gmail.com>
2018-03-27 12:43:58 +0530

    modified connection.php

--- database/connection.php
+++ database/connection.php
@@ -1,5 +1,5 @@
 <?php 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-$conn = mysqli_connect('localhost', 'root', 'root', 'firstjob');
+$conn = mysqli_connect('localhost', 'root', 'admin', 'firstjob');
 ?>
