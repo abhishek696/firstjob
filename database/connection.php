8b4fb27058e57a4689bd66be3767b03a01cd31ae
abhishek696 <abhishektyagi8324@gmail.com>
2018-03-27 14:57:49 +0530

    http://team2.hestalabs.com/

--- database/connection.php
+++ database/connection.php
@@ -1,7 +1,7 @@
 <?php 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-$conn = mysqli_connect('localhost', 'root', 'admin', 'firstjob');
+$conn = mysqli_connect('http://team2.hestalabs.com/', 'root', 'admin', 'firstjob');
 
 if ($conn) {
 	# code...
